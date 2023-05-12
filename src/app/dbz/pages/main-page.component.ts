22d1089f0b96fb12c2f0c3db041dac25f2e46beb
Rodolfo Lopez Lozano <201606416@gmail.com>
2023-05-11 23:34:51 -0400

    module dbz and components

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page-dbz',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent{
+
+}
